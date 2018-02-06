cf1fd49946560ab76326bfd2e2c956c089533ae1
Rahul Sawant <Rahul.Sawant@salebuild.com>
2018-02-06 12:59:58 -0500

    Added check for duplicate username and email and check for empty input

--- app.js
+++ app.js
@@ -24,9 +24,21 @@ app.post('/users', (req, res) => {
         email: req.body.email
     });
 
-    user.save();
-
-    res.send("User created successfully");
+    if(
+        req.body.username == null || req.body.username == '' ||
+        req.body.password == null || req.body.password == '' ||
+        req.body.email == null || req.body.email == ''
+    ){
+        res.send("Ensure username, password and email were provided");
+    } else {
+        user.save(function(err){
+            if(err){
+                res.send('Username or email already exists');
+            } else {
+                res.send("User created successfully");
+            }
+        });
+    }
 });
 
 const port = process.env.PORT || 3000;
